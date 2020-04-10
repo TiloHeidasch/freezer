1f23360323e1ccb913d556ce276d17380f64ef8e
Tilo Heidasch <tilo.heidasch@capgemini.com>
2020-04-10 15:45:43 +0200

    Removes unnecessary log

--- apps/freezer/src/app/app.component.ts
+++ apps/freezer/src/app/app.component.ts
@@ -25,7 +25,6 @@ export class AppComponent {
   }
   setActiveFreezer(freezer: FreezerDto) {
     this.activeFreezer = freezer;
-    console.log(this.activeFreezer);
   }
   createNewFreezer() {
     const dialogRef = this.dialog.open(CreateFreezerDialogComponent);
